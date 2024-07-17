4d34792036c3ccaae9348cfa488801df1866fce7
Lê Thanh Tài <mrtaivietbac@gmail.com>
2024-07-17 17:04:17 +0700

    refix- non completed

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,3 @@
-// Function to open a new tab with a specified URL
-function toUrl(url: string) { 
-    chrome.tabs.create({ url });
-}
-
-// Listen for a click on the extension icon
-chrome.action.onClicked.addListener((tab) => {
-    // Call the toUrl function with the desired URL
-    toUrl('https://www.facebook.com');
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed');
 });
